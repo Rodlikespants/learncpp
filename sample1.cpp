99cf76e4b1e0a3eee5341d5e29354b4e7c39407e
Rodlikespants <rodwittlai@gmail.com>
2013-09-04 17:46:15 -0400

    String practice

--- sample1.cpp
+++ sample1.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <string>
  
 int main()
 {
-    using namespace std;
-    cout << "Hello world!" << endl;
+    using namespace std; // for both cout and string
+    cout << "Enter your name: ";
+    string strString;
+    cin >> strString;
+    cout << "Hello, " << strString << "!" << endl;
+ 
+    cout << "Your name has: " << strString.length() <<
+            " characters in it" << endl;
+    cout << "The 2nd character is: " << strString[1] << endl;
+ 
+    strString = "Dave";
+    cout << "Your name is now " << strString << endl;
+    cout << "Goodbye, " << strString << endl;
+ 
     return 0;
 }
